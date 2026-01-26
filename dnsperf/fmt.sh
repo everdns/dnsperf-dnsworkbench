f5ffe00e96758827f06c73a7eff0984619bd4354
ckranig <ckranig@wisc.edu>
2026-01-26 12:18:29 -0600

    removed superflous code

--- dnsperf/fmt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-clang-format \
-    -style=file \
-    -i \
-    src/*.c \
-    src/*.h
--- dnsperf/src/test/test6.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -xe
-
-test "$TEST_DNSPERF_WITH_NETWORK" = "1" || exit 0
-
-../dnsperf -vvv -d "$srcdir/datafile6" -s 1.1.1.1 >test6.out
-
-grep "NXDOMAIN 26" test6.out
